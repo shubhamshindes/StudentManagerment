6992ca7871e94ab187def59617dc58955688bbc6
shubhamshindes <shubhamshindes.dev@gmail.com>
2025-09-29 13:09:42 +0530

    entity udated

--- StudentManagement/src/main/java/com/sm/entity/Student.java
+++ StudentManagement/src/main/java/com/sm/entity/Student.java
@@ -1,5 +1,26 @@
 package com.sm.entity;
 
-public class Student {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	@Column(name= "first_name",nullable=false)
+	String fname;
+	@Column(name= "last_name",nullable=false)
+	String lname;
+	@Column(name= "email",nullable=false)
+	String email;
+	@Column
+	Integer age;
+	@Column
+	String department;
 }
